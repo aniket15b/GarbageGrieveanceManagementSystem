62ce7eb8a8833a1227a30327fa5097fb1a708d09
aniket15b <aniket15b@gmail.com>
2019-10-20 15:28:05 +0530

    Rearranged files

--- test.py
+++
@@ -1,8 +0,0 @@
-import random
-
-for i in range(1000):
-    list = []
-    
-    dict = {'time': random.randrange(00,23,1), 'location': [random.uniform(19.126067,19.127069),random.uniform(72.033855 ,72.870744)],'date':[random.randrange(1,28,1),random.randrange(1,12,1)]}
-    list.append(dict)
-    print(list)
