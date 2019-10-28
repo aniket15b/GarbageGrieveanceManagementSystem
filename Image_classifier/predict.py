76a24ddebd85af59836a73ab7177c2b70d3c5c0e
aniket15b <aniket15b@gmail.com>
2019-10-28 12:40:04 +0530

    Testing filesaving for heroku

--- Image_classifier/predict.py
+++ Image_classifier/predict.py
@@ -9,9 +9,9 @@ def prepare(file):
     new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
     return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 model = tf.keras.models.load_model("./Image_classifier/CNN.model") #heroku
+print(os.listdir("."))
 image = "./Images/"+os.listdir("./Images")[0]  #heroku
 a = prepare(image)
 prediction = model.predict(a)
 prediction = prediction[0]
 print(prediction[0])
-
